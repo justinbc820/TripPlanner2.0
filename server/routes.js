844f3c389fef60d6a34814f9071f7c679c00b2b4
Vanessa Sergeon <vsergeon@gmail.com>
2014-09-29 17:43:38 -0400

    Added Trip schema. Updated user schema

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/trips', require('./api/trip'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
