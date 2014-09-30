a6b0c14ab415ce0e420b0f2e778e532b677f332e
Jonah Lau <j.nh.lau@gmail.com>
2014-09-30 01:46:23 -0400

    added nodes for questionnaire and nodes endpoint in backend

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/node/node.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
