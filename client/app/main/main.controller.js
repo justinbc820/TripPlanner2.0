4a3bd02cdf1c4182ad523bcf4e0b4eea92d3db8d
Jonah Lau <j.nh.lau@gmail.com>
2014-09-30 15:13:36 -0400

    fixed some login stuff on main.html

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -15,6 +15,7 @@ angular.module('tripPlannerApp')
         Auth.createUser({
           // name: $scope.user.name,
           email: $scope.user.email,
+          name: $scope.user.name,
           password: $scope.user.password
         })
         .then( function() {
@@ -37,5 +38,22 @@ angular.module('tripPlannerApp')
     $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
     };
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+
+
+
+
   });
 
