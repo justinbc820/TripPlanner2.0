df8236e2ee7f1bbcace6b8a18ead7787b3ba3165
Vanessa Sergeon <vsergeon@gmail.com>
2014-09-30 16:58:00 -0400

    plan stuff

---
+++ client/app/plan/plan.controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('tripPlannerApp')
+  .controller('PlanCtrl', function () {
+    this.map = {
+      center: {
+          latitude: 45,
+          longitude: -73
+      },
+      zoom: 8
+    };
+    this.radioModel = 'map';
+    this.eventSources = [];
+    this.uiConfig = {
+      calendar:{
+        height: 800,
+        editable: true,
+        header:{
+          left: 'month basicWeek basicDay agendaWeek agendaDay',
+          center: 'title',
+          right: 'today prev,next'
+        },
+        dayClick: this.alertEventOnClick,
+        eventDrop: this.alertOnDrop,
+        eventResize: this.alertOnResize
+      }
+    };
+  });
