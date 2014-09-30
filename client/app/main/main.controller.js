9eaa86fc48e29dddba521468c26190d98c807abe
Vanessa Sergeon <vsergeon@gmail.com>
2014-09-30 11:53:50 -0400

    Main page sign in form works

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,41 @@
 'use strict';
 
 angular.module('tripPlannerApp')
-  .controller('MainCtrl', function ($scope) {
-    this.thing = "hello";
+  .controller('MainCtrl', function ($scope, Auth, $location, $window) {
+
+    this.someData = "some data";
+
+    $scope.user = {};
+    $scope.errors = {};
+
+    $scope.register = function(form) {
+      $scope.submitted = true;
+
+      if(form.$valid) {
+        Auth.createUser({
+          // name: $scope.user.name,
+          email: $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          // Account created, redirect to home
+          $location.path('/');
+        })
+        .catch( function(err) {
+          err = err.data;
+          $scope.errors = {};
+
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, function(error, field) {
+            form[field].$setValidity('mongoose', false);
+            $scope.errors[field] = error.message;
+          });
+        });
+      }
+    };
+
+    $scope.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
   });
+
