30d71c5b113d1749f0f984704e25a9094e9006f7
Justin <justinbchristensen@gmail.com>
2014-10-02 00:16:59 -0400

    Got autocomplete working on enter and otherwise.  Got search bar to search GooglePlaces if not selected from autocomplete, and to not search GooglePlaces if option is selected from autocomplete

--- client/app/mapOverlay/mapOverlay.controller.js
+++ client/app/mapOverlay/mapOverlay.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tripPlannerApp')
-  .controller('MapOverlayCtrl', function ($scope, planData, ngGPlacesAPI) {
+  .controller('MapOverlayCtrl', function ($scope, $interval, $timeout, planData, ngGPlacesAPI) {
   	// Sets the autocomplete box to search a location with a radius of meters
   	var currentTrip = planData.getCurrentTrip();
     var bounds = new google.maps.Circle({
@@ -9,19 +9,44 @@ angular.module('tripPlannerApp')
     		currentTrip.location.latitude, 
     		currentTrip.location.longitude
     		),
-    	radius: 2000
+    	// radius: 2000
     })
     .getBounds();
 
-    // this.autocomplete;
+    this.details = {};
 
     this.toggleView = function(view) {
     	planData.toggleView(view);
     };
 
     this.placesSearch = function(autocomplete) {
-    	console.log(autocomplete);
-    	
+    	var self = this;
+    	var alreadyDetails = false;
+    	/*
+			If something is selected from the autocomplete list, the user wants that
+			specific location, not 20 search results, so, this function will check 10 
+			times within 500ms to see if there are any place details.  If there aren't,
+			then it's assumed that we should query google for a list of results rather 
+			than just the details of the place that they selected.
+    	*/
+    	var checkForDetails = $interval(function() {
+    		if(self.details.details !== undefined) {
+				$interval.cancel(checkForDetails);
+				console.log(self.details.details);
+				self.details.details = undefined;
+				alreadyDetails = true;
+    		}
+    	}, 50, 10);
+
+		$timeout(function() {
+			if(!alreadyDetails) {
+				ngGPlacesAPI.textSearch({'query':autocomplete})
+					.then(function(data) {
+						console.log(data);
+					});
+			}
+		},500);		
+
     };
 
     this.searchFieldOptions = {
