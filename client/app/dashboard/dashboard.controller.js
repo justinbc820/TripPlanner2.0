b977c4faa59219e3b732385cf3f90ca1f90b2e9f
Justin <justinbchristensen@gmail.com>
2014-10-07 20:43:27 -0400

    working to make dashboard link to individual trip ids

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -3,12 +3,28 @@
 angular.module('tripPlannerApp')
   .controller('DashboardCtrl', function ($scope, Auth, $http, planData, $location) {
     this.getCurrentUser = Auth.getCurrentUser;
-    this.userData = this.getCurrentUser();
+    $scope.userData = this.getCurrentUser();
 
-    $scope.createTrip = function() {
+    this.tripEditView = function(trip) {
+        $http.get('/api/trips/' + trip).success(function(trip) {
+            console.log(trip);
+        })
+    };
+
+    this.createTrip = function() {
+        // This function creates a new trip, saves it in Mongo, then sets the 
+        // current trip variable on planData's scope.
     	$http.post('/api/trips/').success(function(trip) {
     		planData.setCurrentTrip(trip);
-    		$location.path('/map');
+            $http.get('/api/users/me').success(function(user) {
+                var userId = user._id;
+                $http.put('/api/users/' + userId, {
+                    id:trip._id
+                }).success(function(user) {
+                    $scope.userData = user;
+                })
+            })
+    		// $location.path('/map');
     	});
     };
   });
