58d4fb553a78257e0fa330b823d05fae73cb392b
Vanessa Sergeon <vsergeon@gmail.com>
2014-10-14 15:15:10 -0400

    Calendar methods for click, drop, etc are working

--- client/app/calendar/calendar.controller.js
+++ client/app/calendar/calendar.controller.js
@@ -5,22 +5,8 @@ angular.module('tripPlannerApp')
 
     this.init = function() {
       updatePlanData();
-    }
-
-    this.uiConfig = {
-      calendar:{
-        editable: true,
-        // height: 600,
-        header:{
-          left: 'month basicWeek basicDay agendaWeek agendaDay',
-          center: 'title',
-          right: 'today prev,next'
-        },
-        dayClick: this.alertEventOnClick,
-        eventDrop: this.alertOnDrop,
-        eventResize: this.alertOnResize
-      }
     };
+
 
     var date = new Date();
     var d = date.getDate();
@@ -43,7 +29,6 @@ angular.module('tripPlannerApp')
         self.eventSources[0] = self.events.map(function(event) {
           return {title: event.title, start: new Date(event.start)};
         });
-        // $scope.$apply();
       });
     };
 
@@ -67,25 +52,28 @@ angular.module('tripPlannerApp')
     };
 
     this.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [
+      color: '#f00',
+      textColor: 'yellow',
+      events: [
           {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
           {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
           {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
+      ]
     };
     /* alert on eventClick */
     this.alertOnEventClick = function( event, allDay, jsEvent, view ){
-        this.alertMessage = (event.title + ' was clicked ');
+      this.alertMessage = (event.title + ' was clicked ');
+      console.log(this.alertMessage);
     };
     /* alert on Drop */
-     this.alertOnDrop = function( event, revertFunc, jsEvent, ui, view){
-       this.alertMessage = ('Event Droped on ' + event.start.format());
+    this.alertOnDrop = function( event, revertFunc, jsEvent, ui, view){
+     this.alertMessage = ('Event Droped on ' + event.start);
+     console.log(this.alertMessage);
     };
     /* alert on Resize */
     this.alertOnResize = function( event, jsEvent, ui, view){
-       this.alertMessage = ('Event end date was moved to ' + event.end.format());
+     this.alertMessage = ('Event end date was moved to ' + event.end.format());
+     console.log(this.alertMessage);
     };
     /* add and removes an event source of choice */
     this.addRemoveEventSource = function(sources,source) {
@@ -100,23 +88,27 @@ angular.module('tripPlannerApp')
         sources.push(source);
       }
     };
+
     /* add custom event*/
-    this.addEvent = function() {
-      this.events.push({
-        title: 'Open Sesame',
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        className: ['openSesame']
-      });
-    };
+    // this.addEvent = function() {
+    //   this.events.push({
+    //     title: 'Open Sesame',
+    //     start: new Date(y, m, 28),
+    //     end: new Date(y, m, 29),
+    //     className: ['openSesame']
+    //   });
+    // };
+
     /* remove event */
     this.remove = function(index) {
       this.events.splice(index,1);
     };
+
     /* Change View */
     this.changeView = function(view,calendar) {
       calendar.fullCalendar('changeView',view);
     };
+
     /* Change View */
     this.renderCalender = function(calendar) {
       if(calendar){
@@ -139,6 +131,20 @@ angular.module('tripPlannerApp')
     /* event sources array*/
     this.eventSources = [this.events];
     this.eventSources2 = [this.calEventsExt, this.eventsF, this.events];
+
+    this.uiConfig = {
+      calendar:{
+        editable: true,
+        header:{
+          left: 'month agendaWeek agendaDay',
+          center: 'title',
+          right: 'today prev,next'
+        },
+        eventClick: this.alertOnEventClick,
+        eventDrop: this.alertOnDrop,
+        eventResize: this.alertOnResize
+      }
+    };
 
     this.init();
 
